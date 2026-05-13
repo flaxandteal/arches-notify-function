a56e9fa59e64032c6c5be1c60fe8cc3c85bc643d
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2026-05-13 12:42:38 +0100

    fix: remove loaded modules

--- arches_notifications/__init__.py
+++ arches_notifications/__init__.py
@@ -1,4 +1,2 @@
-from .notification_config import NotificationConfig, ResourceNameConfig
-from .notification_base_strategy import NotificationStrategy
-from .notify_function import NotifyFunction
-from .utils import node_id
+
+default_app_config = "arches_notifications.apps.ArchesNotificationsConfig"
